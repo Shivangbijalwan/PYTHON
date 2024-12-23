7a41950c346e81ff6fbc5850b21928442b61a972
Shivang Bijalwan <shivangbijalwan771@gmail.com>
2024-12-23 22:46:34 +0530

    done

---
+++ 15linkedlist.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Creating nodes
+node0 = Node(10)
+node1 = Node(12)
+node2 = Node(14)
+node3 = Node(16)
+node4 = Node(18)
+node5 = Node(20)
+
+# Connecting all nodes together
+node0.next = node1
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+#printing the linked list
+current = node0
+while current is not None:
+    print(current.data , end = "->")
+    current = current.next
+print("None")
