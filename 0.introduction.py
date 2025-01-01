c5f1ec34cdaf121b2fffbaaf3f06fc5cf8f946b2
Shivang Bijalwan <shivangbijalwan771@gmail.com>
2025-01-01 19:22:51 +0530

    commit

--- 0.introduction.py
+++ 0.introduction.py
@@ -3,15 +3,3 @@
 
 #you will learn and develop every part
 
-#1.  Introduction to AI 
-#2.  Introduction to python 
-#3.  Introduction to numpy
-#4.  Introduction to For loops
-#5.  Introduction to While Loops
-#6.  Introduction to Pip
-#7.  Introduction to break and continue
-#8.  Introduction to array
-#9.  Introduction to linked list
-#10. Introduction to stacks
-#11. introduction to Queue
-#12. Introduction to Trees
