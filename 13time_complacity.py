c5bc8dc1b898f392d60952308d16c26d1249b1ec
Shivang Bijalwan <shivangbijalwan771@gmail.com>
2025-01-02 13:27:51 +0530

    upgraded

--- 13time_complacity.py
+++ 13time_complacity.py
@@ -1,8 +1,12 @@
+#wrong approach taken
+
 import time 
 start = time.time()
 for i in range(1,101):
     print(i)
 print(time.time()-start)
 
-#you can check with this method but it take different time in different system 
-# on acc to its configurations
+#you can check with this method but it take different time in different system on acc to its configurations......
+# not for extremely small input
+# time varies have  time problem 
+# time varies if implementation changes.....
