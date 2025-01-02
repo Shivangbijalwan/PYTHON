4a5fa1f0d219779cbf58d76a9c2c71c1da90f96c
Shivang Bijalwan <shivangbijalwan771@gmail.com>
2025-01-02 21:07:59 +0530

    updated

--- 13time_complacity.py
+++ 13time_complacity.py
@@ -1,4 +1,4 @@
-#wrong approach taken
+#wrong approach 1
 
 import time 
 start = time.time()
@@ -9,4 +9,10 @@ print(time.time()-start)
 #you can check with this method but it take different time in different system on acc to its configurations......
 # not for extremely small input
 # time varies have  time problem 
-# time varies if implementation changes.....
+# time varies if implementation changes..... 
+
+#wrong approach 2
+#here we will take the code and count the how many operation taken by the code 
+
+
+#order of growth , it is the graph time and growth 
