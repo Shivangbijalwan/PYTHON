770c31e1feb15873000e079e0bf6d5fced339854
Shivang Bijalwan <shivangbijalwan771@gmail.com>
2025-01-05 07:15:10 +0530

    updated

--- 13time_complacity.py
+++ 13time_complacity.py
@@ -95,5 +95,5 @@ L=[1,2,3,4,5]
 for i in range(0,len(L)):
    for j in range(i + 1,len(L)):
       print({} , {}.format(L[i],L[j]))
-""" """
+"""time complicity of this code is O(N^2) """
 ##########################  Question end  #############################
